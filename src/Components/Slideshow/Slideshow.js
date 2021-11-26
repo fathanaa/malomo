d7ba19be3a2ce2259bfe22bef6c0e7ad705dbdc4
fathanaa <fathan.aa.01@gmail.com>
2021-11-26 13:05:27 +0700

    swiper

--- src/Components/Slideshow/Slideshow.js
+++ src/Components/Slideshow/Slideshow.js
@@ -1,6 +1,8 @@
 import ImageSlide from './ImageSlide'
 import React from 'react'
 import './Slideshow.css'
+import { Swiper, SwiperSlide } from 'swiper/react';
+// import 'swiper/css';
 
 
 const Slideshow = () => {
@@ -24,6 +26,25 @@ const Slideshow = () => {
                 source:'https://raw.githubusercontent.com/fathanaa/malomo/main/src/Asset/Image/Innovation_GA1.png'
             }}/>
 
+            <ImageSlide 
+            data={{
+                name:'Foto1',
+                source:'https://raw.githubusercontent.com/fathanaa/malomo/main/src/Asset/Image/Innovation_GA1.png'
+            }}/>
+
+
+            <Swiper
+                spaceBetween={50}
+                slidesPerView={3}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => console.log(swiper)}
+                >
+                <SwiperSlide>Slide 1</SwiperSlide>
+                <SwiperSlide>Slide 2</SwiperSlide>
+                <SwiperSlide>Slide 3</SwiperSlide>
+                <SwiperSlide>Slide 4</SwiperSlide>
+                ...
+                </Swiper>
            
         </div>
     )
