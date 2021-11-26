c7107d5b78d3ac748b8152971fbc95a6a94a0c1f
severe.Proc <60329788+fathanaa@users.noreply.github.com>
2021-11-26 19:50:45 +0700

    slide

--- src/Components/Slideshow/Slideshow.js
+++ src/Components/Slideshow/Slideshow.js
@@ -1,9 +1,9 @@
 import ImageSlide from './ImageSlide'
 import React from 'react'
 import './Slideshow.css'
-import { Swiper, SwiperSlide } from 'swiper/react';
-// import 'swiper/css';
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/swiper-bundle.min.css'
+import 'swiper/swiper.min.css'
 
 const Slideshow = () => {
     return(
@@ -31,21 +31,20 @@ const Slideshow = () => {
                 name:'Foto1',
                 source:'https://raw.githubusercontent.com/fathanaa/malomo/main/src/Asset/Image/Innovation_GA1.png'
             }}/>
-
-
-            <Swiper
+           
+           <Swiper
                 spaceBetween={50}
                 slidesPerView={3}
-                onSlideChange={() => console.log('slide change')}
-                onSwiper={(swiper) => console.log(swiper)}
+                centeredSlides
+                onSlideChange={() => console.log("slide change")}
+                onSwiper={swiper => console.log(swiper)}
                 >
                 <SwiperSlide>Slide 1</SwiperSlide>
                 <SwiperSlide>Slide 2</SwiperSlide>
                 <SwiperSlide>Slide 3</SwiperSlide>
                 <SwiperSlide>Slide 4</SwiperSlide>
-                ...
-                </Swiper>
-           
+            </Swiper>
+
         </div>
     )
 }
